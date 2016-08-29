3e6258717cbf68cdc5c78763e70ada5328bbb994
lokiiart <lokiiart@gmail.com>
2016-08-30 01:35:08 +0800

    request 可用

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import requests
 
 MyApp = Flask(__name__)
 
 @MyApp.route("/")
 def hello():
-	return "test"
+	r = requests.get("http://www.cpad.gov.cn")
+	return r.text
 
 if __name__ == "__main__":
 	MyApp.run()
